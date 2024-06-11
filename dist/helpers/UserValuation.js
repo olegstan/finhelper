e6bfd52ff565efff5e837eed74553bcf29867d44
olegstan <olegstan@inbox.ru>
2024-06-11 15:17:44 +0300

    aton valuation

--- dist/helpers/UserValuation.js
+++ dist/helpers/UserValuation.js
@@ -3,6 +3,7 @@ import Cache from "./Cache";
 import Active from "./Active";
 import InvestCalc from "./InvestCalc";
 import IndexedDBCache from "./IndexedDBCache";
+import { Api } from "laravel-request";
 class ReactComponentEmulator {
   state = {};
   setState(param, callback) {
@@ -20,6 +21,12 @@ export default class UserValuation {
   static getAccountValuation(array) {
     return array.sum > 0 ? array.sum : 0;
   }
+
+  /**
+   *
+   * @param state
+   * @return {number}
+   */
   static getWholeActivesSum(state) {
     let sum = 0;
 
@@ -32,6 +39,12 @@ export default class UserValuation {
 
     return sum;
   }
+
+  /**
+   *
+   * @param state
+   * @return {number}
+   */
   static getInvestActivesSum(state) {
     let sum = 0;
 
@@ -44,48 +57,75 @@ export default class UserValuation {
 
     return sum;
   }
-  static async getInvestActivesValuation(clientId, currencyId, accountBanks = [], courses) {
-    var component = new ReactComponentEmulator();
-    component.state = {
-      accounts: [],
-      actives: [],
-      invests: [],
-      properties: [],
-      spendings: [],
-      obligations: [],
-      cashBalance: {
-        sum: 0
-      },
-      bankBalance: {
-        sum: 0
-      },
-      brokerBalance: {
-        sum: 0
-      },
-      digitBalance: {
-        sum: 0
-      }
-    };
-    let cachedValue = await IndexedDBCache.get('client.valuation.' + clientId);
-    if (cachedValue !== null) {
-      return cachedValue;
-    } else {
+
+  /**
+   *
+   * @param clientId
+   * @param currencyId
+   * @param accountBanks
+   * @param courses
+   * @param atonValuation
+   * @return {Promise<unknown>}
+   */
+  static async getInvestActivesValuation(clientId, currencyId, accountBanks = [], courses, atonValuation = false) {
+    if (atonValuation) {
       return await new Promise((resolve, reject) => {
-        let now = moment();
-        let currencyData = {
-          currency_id: currencyId,
+        Api.get('aton-portfolio', 'index', {
           user_id: clientId
-        };
-        Active.getAccountsByDate(component, 'accounts', currencyData, clientId, accountBanks, now, () => {
-          Active.getInvestsByDate(component, 'invests', currencyData, clientId, accountBanks, now, () => {
-            let valuation = UserValuation.getInvestActivesSum(component.state);
-            if (valuation > 0) {
-              Cache.setItem('client.valuation.' + clientId, valuation);
-            }
-            resolve(valuation);
+        }).setDomain(process.env.REACT_APP_API_WHITESWAN_URL).all(({
+          data
+        }) => {
+          let sum = 0;
+          data.map(row => {
+            sum += row.score_position_rub;
           });
+          resolve(sum);
         });
       });
+    } else {
+      //если данные
+      var component = new ReactComponentEmulator();
+      component.state = {
+        accounts: [],
+        actives: [],
+        invests: [],
+        properties: [],
+        spendings: [],
+        obligations: [],
+        cashBalance: {
+          sum: 0
+        },
+        bankBalance: {
+          sum: 0
+        },
+        brokerBalance: {
+          sum: 0
+        },
+        digitBalance: {
+          sum: 0
+        }
+      };
+      let cachedValue = await IndexedDBCache.get('client.valuation.' + clientId);
+      if (cachedValue !== null) {
+        return cachedValue;
+      } else {
+        return await new Promise((resolve, reject) => {
+          let now = moment();
+          let currencyData = {
+            currency_id: currencyId,
+            user_id: clientId
+          };
+          Active.getAccountsByDate(component, 'accounts', currencyData, clientId, accountBanks, now, () => {
+            Active.getInvestsByDate(component, 'invests', currencyData, clientId, accountBanks, now, () => {
+              let valuation = UserValuation.getInvestActivesSum(component.state);
+              if (valuation > 0) {
+                Cache.setItem('client.valuation.' + clientId, valuation);
+              }
+              resolve(valuation);
+            });
+          });
+        });
+      }
     }
   }
 }
